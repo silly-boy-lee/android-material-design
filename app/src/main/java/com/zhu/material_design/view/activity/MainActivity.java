a61a8f5c6532345eacb5243430f8856d10daeabc
Mr.Lee <2932582804@qq.com>
2017-03-16 17:31:05 +0800

    工程建立，首次提交

--- app/src/main/java/com/zhu/material_design/view/activity/MainActivity.java
+++ app/src/main/java/com/zhu/material_design/view/activity/MainActivity.java
@@ -4,12 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.zhu.material_design.R;
+import com.zhu.material_design.common.base.activity.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+
+
+    @Override
+    protected int initLayout() {
+        return R.layout.activity_main;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
